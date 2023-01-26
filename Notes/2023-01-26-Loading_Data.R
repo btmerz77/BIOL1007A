d8ab2ea02cb664c4a9c53b84768ca8cecae982de
btmerz77 <btmerz@middlebury.edu>
2023-01-26 15:27:01 -0500

    t #	renamed:    Assignment_1.html -> Daily_Assignments/Assignment_1.html

---
+++ Notes/2023-01-26-Loading_Data.R
@@ -0,0 +1,10 @@
+##### Loading Data
+##### 26 January 2023
+##### BTM
+
+### write.table()
+  # allows you to add # at the beggining of the file for meta data
+write.table(x=my_data,
+            file="Path/To/OutputFileName.csv",
+            HEADER=TRUE,
+            sep=",")
